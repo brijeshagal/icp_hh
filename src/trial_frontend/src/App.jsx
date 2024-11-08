99b6a2935b9578225a6b3b752d83bb53ef12121b
brijeshagal <brijeshagal@gmail.com>
2024-11-08 19:46:50 +0700

    add bitfinity

--- src/trial_frontend/src/App.jsx
+++ src/trial_frontend/src/App.jsx
@@ -3,19 +3,17 @@ import { useState } from "react";
 import HelloAbi from "../public/abi.json";
 // canister id: vsr4k-oyaaa-aaaah-aq23a-cai
 // Sepolia address: 0xFC9074F7360D8C8ca4688d0AC9dCf982C3459aa2
+// Bitfinity contract address: 0xdBB537aAd87536Fb5D74aAE4478ace7E10ac7AF6
+
 function App() {
   const [greeting, setGreeting] = useState("");
   const [submittedNames, setSubmittedNames] = useState([]);
-
+  const bitfinityContract = "0xdBB537aAd87536Fb5D74aAE4478ace7E10ac7AF6";
   async function handleSubmit() {
     try {
       const provider = new ethers.BrowserProvider(window.ethereum);
       const signer = await provider.getSigner();
-      const contract = new ethers.Contract(
-        "0xFC9074F7360D8C8ca4688d0AC9dCf982C3459aa2",
-        HelloAbi,
-        signer
-      );
+      const contract = new ethers.Contract(bitfinityContract, HelloAbi, signer);
       const res = await contract.greet(greeting);
       await res.wait();
       getSubmittedNames();
@@ -26,14 +24,12 @@ function App() {
 
   async function getSubmittedNames() {
     // const provider = new ethers.BrowserProvider(window.ethereum);
-    const provider = new ethers.JsonRpcProvider("https://eth-sepolia.public.blastapi.io");
+    const provider = new ethers.JsonRpcProvider(
+      "https://testnet.bitfinity.network"
+    );
     // const signer = await provider.getSigner();
     // console.log("Account:", await signer.getAddress());
-    const contract = new ethers.Contract(
-      "0xFC9074F7360D8C8ca4688d0AC9dCf982C3459aa2",
-      HelloAbi,
-      provider
-    );
+    const contract = new ethers.Contract(bitfinityContract, HelloAbi, provider);
     const res = await contract.getSubmittedNames();
     setSubmittedNames(res);
   }
